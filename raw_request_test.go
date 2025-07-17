f9dbed49bd361fc7fabdcb644ecffc3c01be244e
Hubert Chen <01@trle5.xyz>
2025-07-17 22:31:39 +0800

    add hide bot token test

--- raw_request_test.go
+++ raw_request_test.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"context"
+	"errors"
 	"io"
 	"net/http"
 	"strings"
@@ -14,6 +15,9 @@ type clientMock struct {
 
 func (c *clientMock) Do(req *http.Request) (*http.Response, error) {
 	c.requestURI = req.URL.RequestURI()
+	if c.requestURI == "failed/botXXX/foo" {
+		return nil, errors.New(c.requestURI)
+	}
 	resp := http.Response{
 		StatusCode: 200,
 		Body:       io.NopCloser(strings.NewReader(`{"ok":true}`)),
@@ -53,5 +57,23 @@ func Test_rawRequest_url_testEnv(t *testing.T) {
 
 	if cm.requestURI != "/botXXX/test/foo" {
 		t.Fatalf("unexpected requestURI: %s", cm.requestURI)
+	}
+}
+
+func Test_rawRequest_err_hideToken(t *testing.T) {
+	cm := &clientMock{}
+	b := &Bot{
+		url:    "failed",
+		token:  "XXX",
+		client: cm,
+	}
+
+	err := b.rawRequest(context.Background(), "foo", nil, nil)
+	if err == nil {
+		t.Fatalf("unexpected nil error")
+	}
+
+	if strings.Contains(err.Error(), "XXX") {
+		t.Fatalf("unexpected error with token: %s", err.Error())
 	}
 }
